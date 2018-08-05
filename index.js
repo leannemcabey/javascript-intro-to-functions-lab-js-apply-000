8c0d85b39e3d912ab2a122d2fa2e4abb2d8408de
Leanne Cabey <leanne.m.cabey@gmail.com>
2018-08-05 01:22:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === string)
+  if (string.toLowerCase() === string) {
+    return "I can't hear you!"
+  }
+  
 }
+
